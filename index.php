91e39ecad5e661b93e2af0349904a3f3150f0316
Robert Convery <Rob.Convery@zerocarbonfuture.com Rob.Convery@zerocarbonfuture.com>
2014-11-07 12:32:02 +0000

    changed index file.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-
-phpinfo();
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My GitHub Test</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>This is My GitHub Test</h1>
+        <div>Welcome to my GitHub test page.</div>
+    </body>
+</html>
